de04945f94f7e783706bcc388cca457792b9b34f
saurabhpatel101 <90132902+saurabhpatel101@users.noreply.github.com>
2022-03-26 21:53:52 +0530

    source

--- src/home.js
+++ src/home.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import './App.css';
 
 const Home = () => {
   return (
-    <div>
+    <div id='calculate'>
         <Link to="/factorial"> <button>Calculate</button></Link>
     </div>
   )
